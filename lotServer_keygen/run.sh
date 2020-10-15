2b20c530607e3493d4e5a1b942327bd2c41be6bb
atm <zzs0@zzs0.com>
2020-10-15 15:51:45 +0800

    新增认证服务器docker

---
+++ lotServer_keygen/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --name keygen -dt -p 4000:4000 acgpiano/server_speeder_keygen:latest
---
+++ lotServer_keygen/src/Actions/AcServlet.js
@@ -0,0 +1,7 @@
+module.exports = function (req, res) {
+  return res.json({
+    code: 200,
+    rdm: Math.floor(Math.random() * 500000 + 1),
+    message: "ok"
+  });
+};
